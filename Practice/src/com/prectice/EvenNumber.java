acf9acc1536be2f0fdd5bfdcf969416a8d1ed895
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2025-07-04 17:15:46 +0530

    practise

---
+++ Practice/src/com/prectice/EvenNumber.java
@@ -0,0 +1,15 @@
+package com.prectice;
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		int n = 100;
+//		for (int j = 2; j <= n; i += 2) {
+//			if (i % 2 == 0)
+//				System.out.println(j);
+//		}
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 0)
+				System.out.println(i);
+		}
+	}
+}
---
+++ Practice/src/com/prectice/FactorOrDiviser.java
@@ -0,0 +1,20 @@
+package com.prectice;
+
+import java.util.Scanner;
+
+public class FactorOrDiviser {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Number");
+		int num = sc.nextInt();
+//		for (int i = 1; i <= num; i++) 
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+
+		}
+		System.out.println(num);
+	}
+}
