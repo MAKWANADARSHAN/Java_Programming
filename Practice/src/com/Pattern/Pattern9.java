a83223027df4f655cb61f2f937017a6464bb3492
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2025-07-29 13:13:24 +0530

    Pattern

--- Practice/src/com/Pattern/Pattern9.java
+++ Practice/src/com/Pattern/Pattern9.java
@@ -2,9 +2,11 @@ package com.Pattern;
 
 public class Pattern9 {
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
+		int n = 1;
+		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
-				System.out.print((i + j) % 2 + " ");
+				System.out.print(n % 2 + " ");
+				n++;
 			}
 
 			System.out.println();
